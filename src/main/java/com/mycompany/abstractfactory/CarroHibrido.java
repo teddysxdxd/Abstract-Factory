dac7146ead77f41b0065365ac3ab035a8d0c7a54
teddy <tedocastel@gmail.com>
2024-09-09 20:11:28 -0600

    Factory Method

---
+++ src/main/java/com/mycompany/abstractfactory/CarroHibrido.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.abstractfactory;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CarroHibrido implements Carro {
+
+    @Override
+    public void acelera(String acelera) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void frena(String frena) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void enciende(String enciende) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+    
+}
