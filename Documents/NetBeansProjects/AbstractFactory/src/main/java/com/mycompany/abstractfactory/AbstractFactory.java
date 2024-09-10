30a014e80191d18a0e80de84ae40255da56b2c53
teddy <tedocastel@gmail.com>
2024-09-09 20:06:51 -0600

    Factory Method

---
+++ Documents/NetBeansProjects/AbstractFactory/src/main/java/com/mycompany/abstractfactory/AbstractFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abstractfactory;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class AbstractFactory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Documents/NetBeansProjects/AbstractFactory/src/main/java/com/mycompany/abstractfactory/Carro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.abstractfactory;
+
+/**
+ *
+ * @author USUARIO
+ */
+public abstract interface Carro {
+    public void acelera (String acelera);
+    public void frena (String frena);
+    public void enciende (String enciende);
+            
+}
